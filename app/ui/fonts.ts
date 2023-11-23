c3164d168395b6f23b5cac48282435df3bb0c7e7
draconids <draconids@nate.com>
2023-11-23 09:46:01 +0900

    feat: font and image

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Nanum_Gothic } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const nanumGothic = Nanum_Gothic({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
