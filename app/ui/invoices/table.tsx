319c8bd9f6ab8494b76503c63d427ef3194756eb
draconids <draconids@nate.com>
2023-11-25 07:03:27 +0900

    fix: error

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
-import InvoiceStatus from '@/app/ui/invoices/status';
-import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
+import { formatCurrency, formatDateToLocal } from '@/app/lib/utils';
+import { DeleteInvoice, UpdateInvoice } from '@/app/ui/invoices/buttons';
+import InvoiceStatus from '@/app/ui/invoices/status';
+import Image from 'next/image';
 
 export default async function InvoicesTable({
   query,
@@ -29,6 +29,7 @@ export default async function InvoicesTable({
                       <Image
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
+                        alt=""
                         width={28}
                         height={28}
                       />
@@ -89,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt=""
                       />
                       <p>{invoice.name}</p>
                     </div>
